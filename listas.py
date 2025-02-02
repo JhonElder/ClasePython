222b4aae1978415f84690076e8101f36dc28a347
JhonElder <elder2588@hotmail.com>
2025-02-02 16:38:49 -0500

    quinto commit

--- listas.py
+++ listas.py
@@ -1,6 +1,7 @@
 #Una lista es una colección ordenada y mutable que permite elementos duplicados.
 # Definiendo una lista
 frutas = ["manzana", "banana", "cereza"]
+numeros = [1, 2, 3, 4, 5, 6, 7, 8]
 
 # Agregar un elemento
 frutas.append("naranja")
@@ -16,3 +17,7 @@ frutas[0] = "kiwi"
 frutas.remove("cereza")
 
 print(frutas)  # Output: ['kiwi', 'banana', 'naranja']
+resultado = frutas + numeros
+print(resultado)
+
+
